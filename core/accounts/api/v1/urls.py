2d19bf362574c3eacdd31da5a3e7540311443366
Mohsen Pourpak <mohsenpourpak@yahoo.com>
2023-09-02 20:47:48 +0330

    jwt token has started

---
+++ core/accounts/api/v1/urls.py
@@ -0,0 +1,27 @@
+# from rest_framework.authtoken.views import ObtainAuthToken
+from django.urls import path, include
+from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+
+app_name = 'api-v1'
+
+urlpatterns = [
+    # registration
+    path('registration/', views.RegistrationApiView.as_view(),name='registration'),
+    path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
+    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout'),
+    # change pass
+    # reset pass
+    # login token
+    # path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/create/', views.CustomTokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt_refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt_verify'),
+
+    # login jwt
+    ]
